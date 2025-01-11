1bfb181f4813b020cae5bdf71da63960b15952a7
Yehia Tarek <92787505+yehia-tarek@users.noreply.github.com>
2025-01-11 11:42:52 +0200

    Add localization support using Mcamara Laravel Localization package

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,22 +12,36 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
         then: function () {
-            Route::prefix('admin')
-                ->middleware(['web', 'use_admin_guard'])
+            Route::prefix(LaravelLocalization::setLocale() . '/admin')
+                ->middleware([
+                    'web',
+                    'use_admin_guard',
+                    'localeSessionRedirect',
+                    'localizationRedirect',
+                    'localeViewPath'
+                ])
                 ->group(__DIR__ . '/../routes/admin.php');
         }
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->prepend([
-
-        ]);
+        $middleware->prepend([]);
 
         $middleware->alias([
+            // admin middleware
             'admin' => App\Http\Middleware\Backend\Auth\AdminMiddleware::class,
+            'use_admin_guard' => \App\Http\Middleware\Backend\Auth\UseAdminGuard::class,
+
+            // Spatie Permission Middleware
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
-            'use_admin_guard' => \App\Http\Middleware\Backend\Auth\UseAdminGuard::class,
+
+            // localization
+            'localize'                => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
+            'localizationRedirect'    => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
+            'localeSessionRedirect'   => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
+            'localeCookieRedirect'    => \Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect::class,
+            'localeViewPath'          => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- routes/web.php
+++ routes/web.php
@@ -4,35 +4,43 @@ use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::group([
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'],
+], function () {
+    
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__ . '/auth.php';
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 
-Route::fallback(function () {
-    if (Auth::guard('admin')->check()) {
-        return redirect()->route('admin.dashboard');
-    }
+    require __DIR__ . '/auth.php';
 
-    if (str_contains(URL::current(), 'admin')) {
-        return redirect()->route('admin.login');
-    }
+    Route::fallback(function () {
+        if (Auth::guard('admin')->check()) {
+            return redirect()->route('admin.dashboard');
+        }
 
-    if (Auth::guard('web')->check()) {
-        return redirect()->route('dashboard');
-    }
+        if (str_contains(URL::current(), 'admin')) {
+            return redirect()->route('admin.login');
+        }
 
-    return redirect()->route('login');
+        if (Auth::guard('web')->check()) {
+            return redirect()->route('dashboard');
+        }
+
+        return redirect()->route('login');
+    });
 });
